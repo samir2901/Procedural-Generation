87a53db4224e34e0320ee8c184afc93acec04ea6
samir2901 <thunder.cse2018@gmail.com>
2020-05-07 10:16:21 +0530

    Initial commit

---
+++ Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int width = 256, height = 256;
+    public float scale = 11f;
+    public float offsetX = 0f, offsetY = 0f;
+    Renderer renderer;
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    private void Update()
+    {
+        
+        renderer.material.mainTexture = GenerateTexture();
+    }
+
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                float xCoord = (float)i / width;
+                float yCoord = (float)j / height;
+                float s = Mathf.PerlinNoise(xCoord * scale + offsetX, yCoord * scale + offsetY);
+                Color color = new Color(s,s,s);
+                texture.SetPixel(i, j, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Procedural-Generation
+ Procedural Generation in Computer Graphics Using Unity
