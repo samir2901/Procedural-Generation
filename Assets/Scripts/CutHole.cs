c5bb32e9645856d6781268ec5fe8b56f71ae7f59
samir2901 <thunder.cse2018@gmail.com>
2020-05-31 20:08:55 +0530

    CutHole in Runtime

---
+++ Assets/Scripts/CutHole.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutHole : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 1000f))
+            {
+                deleteTriangle(hit.triangleIndex);
+                Debug.Log(hit.triangleIndex);
+            }
+        }
+    }
+
+    void deleteTriangle(int index)
+    {
+        Destroy(this.gameObject.GetComponent<MeshCollider>());
+        Mesh mesh = transform.GetComponent<MeshFilter>().mesh;
+        int[] oldTriangles = mesh.triangles;
+        int[] newTriangles = new int[mesh.triangles.Length - 3];
+
+        int i = 0, j = 0;
+
+        while (j < mesh.triangles.Length)
+        {
+            if(j != index * 3)
+            {
+                newTriangles[i++] = oldTriangles[j++];
+                newTriangles[i++] = oldTriangles[j++];
+                newTriangles[i++] = oldTriangles[j++];
+            }
+            else
+            {
+                j += 3;
+            }
+        }
+        transform.GetComponent<MeshFilter>().mesh.triangles = newTriangles;
+        this.gameObject.AddComponent<MeshCollider>();
+    }
+}
